04f5280c05463e97fdb4b8b10fc50a63a9d61fb2
Karol Szuster <kszuster@protonmail.com>
2023-05-12 09:51:39 +0200

    Remove `recovery_email` column from `CloudIdentity`

--- environment/models.py
+++ environment/models.py
@@ -14,7 +14,6 @@ class CloudIdentity(models.Model):
         max_length=255, unique=True, validators=[EmailValidator()]
     )
     initial_workspace_setup_done = models.BooleanField(default=False)
-    recovery_email = models.CharField(max_length=255)
 
 
 class BillingSetup(models.Model):
