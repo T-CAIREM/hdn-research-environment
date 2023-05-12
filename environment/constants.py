c82b424f991937d929bf8e6c491aebf8afd00b35
Karol Szuster <kszuster@protonmail.com>
2023-05-12 10:10:36 +0200

    Reformat code with `black`

--- environment/constants.py
+++ environment/constants.py
@@ -11,9 +11,7 @@ MAX_CPU_USAGE = 32
 
 PERSISTENT_DATA_DISK_NAME = "Persistent data disk 1GB"
 
-ProjectedWorkbenchCost = namedtuple(
-    "ProjectedWorkbenchCost", "resource cost"
-)
+ProjectedWorkbenchCost = namedtuple("ProjectedWorkbenchCost", "resource cost")
 INSTANCE_PROJECTED_COSTS = {
     Region.US_CENTRAL: [
         ProjectedWorkbenchCost(*parameters)
@@ -82,7 +80,9 @@ GPU_PROJECTED_COSTS = {
 
 DATA_STORAGE_PROJECTED_COSTS = {
     Region.US_CENTRAL: ProjectedWorkbenchCost(PERSISTENT_DATA_DISK_NAME, 0.05),
-    Region.NORTHAMERICA_NORTHEAST: ProjectedWorkbenchCost(PERSISTENT_DATA_DISK_NAME, 0.05),
+    Region.NORTHAMERICA_NORTHEAST: ProjectedWorkbenchCost(
+        PERSISTENT_DATA_DISK_NAME, 0.05
+    ),
     Region.EUROPE_WEST: ProjectedWorkbenchCost(PERSISTENT_DATA_DISK_NAME, 0.05),
     Region.AUSTRALIA_SOUTHEAST: ProjectedWorkbenchCost(PERSISTENT_DATA_DISK_NAME, 0.05),
 }
