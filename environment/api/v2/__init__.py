e7a64a44baf819a7d2c8085be286f265514665ba
Karol Szuster <kszuster@protonmail.com>
2023-05-12 12:05:45 +0200

    Add `api.v2.create_cloud_identity`

---
+++ environment/api/v2/__init__.py
@@ -0,0 +1,21 @@
+from environment.api.decorators import api_request
+
+from requests import Request
+
+
+@api_request
+def create_cloud_identity(
+    gcp_user_id: str,
+    given_name: str,
+    family_name: str,
+    password: str,
+    recovery_email: str,
+) -> Request:
+    json = {
+        "user_name": gcp_user_id,
+        "password": password,
+        "given_name": given_name,
+        "family_name": family_name,
+        "recovery_email": recovery_email,
+    }
+    return Request("POST", url="/identity/create", json=json)
