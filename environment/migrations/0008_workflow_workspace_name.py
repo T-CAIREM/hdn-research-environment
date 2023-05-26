97f4d453a931df6766402de35d5437c56d8d9e91
Karol Szuster <kszuster@protonmail.com>
2023-05-26 15:54:36 +0200

    Fix multiple leaf migrations

--- environment/migrations/0008_workflow_workspace_name.py
+++ environment/migrations/0008_workflow_workspace_name.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("environment", "0005_workflow"),
+        ("environment", "0007_billingaccountsharinginvite"),
     ]
 
     operations = [
