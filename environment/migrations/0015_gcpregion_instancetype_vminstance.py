8ec6c7f4bc30b69debc4c5b741933db51d84f0f3
rutvikrj26 <rutvikrj26@gmail.com>
2024-07-03 21:36:23 -0400

    updated migrations for fixing the leaf migrations formation

--- environment/migrations/0015_gcpregion_instancetype_vminstance.py
+++ environment/migrations/0015_gcpregion_instancetype_vminstance.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("environment", "0013_bucketsharinginvite_type_and_more"),
+        ("environment", "0014_googlegroup_cloudidentity_user_groups"),
     ]
 
     operations = [
